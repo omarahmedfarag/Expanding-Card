a0971d66c0429d624e2cb046a5bc4f2bbbde4df9
omarahmedfarag <omarfarag429@gmail.com>
2021-03-27 14:57:34 +0200

    scaled image

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
         e.addEventListener("click",()=>{
             resetWidth();
             e.style.width="65%"
+            e.classList.add("scale-images")
             e.children[1].style.opacity="1"
             e.children[1].style.transition="1s"
         })
@@ -14,8 +15,10 @@
     {
         cards.forEach(e=>{
            e.style.width="7%"
+           e.classList.remove("scale-images")
            e.children[1].style.opacity="0"
            e.children[1].style.transition="none"
+
         })
     }
 
